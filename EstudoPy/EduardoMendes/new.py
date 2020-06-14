f90f0faf33b5a68bdc29873f6b8838c10ae6ac73
mlobf <marcoslemeborbafilho@gmail.com>
2020-06-13 22:01:14 -0300

    New comment on board

--- EstudoPy/EduardoMendes/new.py
+++ EstudoPy/EduardoMendes/new.py
@@ -6,8 +6,12 @@ navegador = Firefox()
 
 navegador.get(url)
 
-a = navegador.find_element_by_tag_name("a")
+a = navegador.find_element_by_tag_name('a')
 
 a.click()
+
+
+#for x in range(10):
+#a.click()
 
 #    navegador.quit()
