46996830d6ae5fdb90c813e20a924b97f697ac45
mlobf <marcoslemeborbafilho@gmail.com>
2020-06-13 21:51:50 -0300

    Segue ai mais uma mensagem

--- EstudoPy/EduardoMendes/new.py
+++ EstudoPy/EduardoMendes/new.py
@@ -1,1 +1,13 @@
-# New to scrap
+from selenium.webdriver import Firefox
+
+url = "https://curso-python-selenium.netlify.app/aula_03.html"
+
+navegador = Firefox()
+
+navegador.get(url)
+
+a = navegador.find_element_by_tag_name("a")
+
+a.click()
+
+#    navegador.quit()
