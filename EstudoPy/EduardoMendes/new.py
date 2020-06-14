cbf0068661463ba9c6ab36f0d8d3fb5453df6923
mlobf <marcoslemeborbafilho@gmail.com>
2020-06-13 22:14:52 -0300

    Ao vai mais uma

--- EstudoPy/EduardoMendes/new.py
+++ EstudoPy/EduardoMendes/new.py
@@ -1,17 +1,21 @@
 from selenium.webdriver import Firefox
+import pdb
+import time
 
 url = "https://curso-python-selenium.netlify.app/aula_03.html"
-
 navegador = Firefox()
 
 navegador.get(url)
 
-a = navegador.find_element_by_tag_name('a')
+pdb.set_trace()
 
-a.click()
+a = navegador.find_element_by_tag_name("a")
 
+for x in range(28):
+    a.click()
+    time.sleep(1)
 
-#for x in range(10):
-#a.click()
+# for x in range(10):
+# a.click()
 
 #    navegador.quit()
