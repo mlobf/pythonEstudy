ece9c263cfdbff856496836a68b0af5894cbe981
mlobf <marcoslemeborbafilho@gmail.com>
2020-06-17 22:29:59 -0300

    Commit da noite,

--- EstudoPy/EduardoMendes/new.py
+++ EstudoPy/EduardoMendes/new.py
@@ -1,16 +1,30 @@
-from selenium.webdriver import Firefox
+from selenium.webdriver import Chrome
 from time import sleep
 
+
+
+
+chrome = Chrome()
+
+
+
+
+
+urls_letra = []
 lista_noticias = []
-firefox = Firefox()
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+           'p','q','r','s','t','u','v','x',]
+
+for letter in letters: url2 = 'https://www.yellowpages.ae/categories-by-alphabet/{}.html'
 
 
-url ='https://www.uol.com.br'
 
 
-firefox.get(url)
+url = "https://www.yellowpages.ae/companies-by-alphabet/a.html"
 
-elementos = firefox.find_elements_by_tag_name("a")
+
+
+elementos = chrome.find_elements_by_tag_name("a")
 
 for elemento in elementos:
     lista_noticias.append(elemento.text)
@@ -19,11 +33,6 @@ sleep(1)
 print(lista_noticias)
 
 
-firefox.quit()
+chrome.quit()
 
 
-
-
-
-
-# p = navegador.find_element_by_tag_name("p")
