a45847bd2feabe46d296ca323a18a742c881d1ec
mlobf <marcoslemeborbafilho@gmail.com>
2020-07-05 22:53:05 -0300

    Commit da noite.

--- EstudoPy/django3UdemyCourse/personal_portfolio_project/personal_portfolio/urls.py
+++ EstudoPy/django3UdemyCourse/personal_portfolio_project/personal_portfolio/urls.py
@@ -17,11 +17,19 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+"Tentativa de criar uma view 21.23 "
+from portfolio import views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
 ]
 
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
