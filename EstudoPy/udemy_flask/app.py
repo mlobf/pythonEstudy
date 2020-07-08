b3684575f214c5394a913788d7a788b273b33e95
mlobf <marcoslemeborbafilho@gmail.com>
2020-07-08 01:57:18 -0300

    Commit da madrugada.

--- EstudoPy/udemy_flask/app.py
+++ EstudoPy/udemy_flask/app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello World"
+stores = [
+    {
+        'name': 'My wonderfull Store',
+        'items': [
+            {
+                'name': 'My Item',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+# POST / store data: {name:}
+@app.route("/store", methods=["POST"])
+def create_store():
+    return ""
+
+# GET / store/<string:name>
+
+
+@app.route("/store/<string:name>")
+def get_store(name):
+    return ""
+
+
+# GET /store
+@app.route("/store/")
+def get_stores():
+    return ""
+
+# POST / store/<string:name>item {name:, price:}
+
+
+@app.route("/store/<string:name>/item", methods=["POST"])
+def create_item_in_store():
+    return ""
+
+# GET /store/<string:name>/item
+
+
+@app.route("/store/<string:name>/item")
+def get_item_in_store():
+    return ""
 
 
 app.run(port=5000)
