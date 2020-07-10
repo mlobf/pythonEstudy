b6c0f9ace3ea2f9a4ee1cb4e32b80e543889a82a
marcos <marcoslemeborbafilho@gmail.com>
2020-07-10 10:01:48 -0300

    Commit da manha

--- EstudoPy/django3UdemyCourse/personal_portfolio_project/personal_portfolio/urls.py
+++ EstudoPy/django3UdemyCourse/personal_portfolio_project/personal_portfolio/urls.py
@@ -20,16 +20,11 @@ from django.conf import settings
 from portfolio import views
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('blog/', include('blog.urls')),
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("blog/", include("blog.urls")),
 ]
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
--- EstudoPy/udemyFlask/app.py
+++ EstudoPy/udemyFlask/app.py
@@ -1,19 +1,22 @@
-#This is simple app
-
+# This is simple app
 from flask import Flask
 
 
 app = Flask(__name__)
 
-#Post  - receive data
-#Get  - send data back only
-@app.route('/store', methods=['POST'])
+# Post  - receive data
+# Get  - send data back only
+@app.route("/store", methods=["POST"])
 def create_store():
     pass
+
+
 # GET/store/<string:name>
-@app.route('/store/<string:name>')
+@app.route("/store/<string:name>")
 def get_store(name):
     pass
+
+
 # GET/store
 # POST/store/<string:name>/item{name:,price:}
 # GET /store/<string:name>/item
