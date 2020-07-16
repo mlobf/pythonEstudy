84ea5e5fd796a3339e3e5aa6333b3b12a85e698c
marcos <marcoslemeborbafilho@gmail.com>
2020-07-16 15:16:24 -0300

    commit tarde

---
+++ EstudoPy/django3UdemyCourse/todotwo-project/todo/exercicio.py
@@ -0,0 +1,25 @@
+""" 
+1- Sortear 6 numeros de 1 a 60. Armazenar em uma lista.
+2- Pedir para o usuario digitar  6 numeros. Armazenar em uma lista.
+3- Checar se o usuario acertou 6, 5 ou 4 numeros.
+4- Checar se o usuario acertou algum numero e informar os numeros sorteados.
+"""
+from random import randrange
+
+numeros_sorteados = []
+numeros_cliente = []
+
+def sortiar_numeros():
+    for x in range(6):
+        numeros_sorteados.append(randrange(0,60))
+    print(numeros_sorteados)
+
+for x in range(6):
+    new_number = input("Coloque o numero")
+    if new_number in numeros_cliente:
+        pass
+        #print("escolha outro numero")
+    
+    #numeros_cliente.append(input("Coloque o numero"))
+
+print(numeros_cliente)
--- EstudoPy/django3UdemyCourse/todotwo-project/todo/views.py
+++ EstudoPy/django3UdemyCourse/todotwo-project/todo/views.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 
 def signupuser(request):
     if request.method == "GET":
-        return render(request, "todo/signupuser.html",
-                      {"form": UserCreationForm()})
+        return render(request, "todo/signupuser.html", {"form": UserCreationForm()})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], 
-                                            password=request.POST['password1'])
+        if request.POST["password1"] == request.POST["password2"]:
+            user = User.objects.create_user(
+                request.POST["username"], password=request.POST["password1"]
+            )
             user.save()
         else:
             print("Be aweare on the enemy of light")
