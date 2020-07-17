d2bc375eddbb5148069e397dc3b37966c7a79bc1
marcos <marcoslemeborbafilho@gmail.com>
2020-07-16 22:00:07 -0300

    Commit da noite

--- EstudoPy/djangoBig/blog/posts/models.py
+++ EstudoPy/djangoBig/blog/posts/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+# Create your models here.
+
 class Post(models.Model):
-    title = models.CharField(max_lenght=50)
+
+    title = models.CharField(max_length=50)
     content = models.TextField()
     date = models.DateField()
 
