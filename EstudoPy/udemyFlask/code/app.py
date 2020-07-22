f9723732f1f06d3d700ad5eb5083d9a5382376dd
marcos <marcoslemeborbafilho@gmail.com>
2020-07-22 13:17:48 -0300

    flaskapibeg

---
+++ EstudoPy/udemyFlask/code/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Student(Resource):
+    def get(self, name):
+        return {"student": name}
+
+
+#    def post(self):
+
+api.add_resource(Student, "/student/<string:name>")
+
+api.run(port=5000)
