0c2ace4ca1291b01af90b081854397846d2047a0
marcos <marcoslemeborbafilho@gmail.com>
2020-07-27 18:05:52 -0300

    segue

---
+++ EstudoPy/oppPython/game/main.py
@@ -0,0 +1,17 @@
+import player
+
+"""
+    getter is an method used to get value of date atribute.
+    setter is just a method used to set the value of the class data atribute
+"""
+tim = player.Player("Tim")
+print(tim.name)
+print(tim.lives)
+
+
+
+
+# tim.lives = 9
+
+# print(tim)
+
--- EstudoPy/oppPython/game/player.py
+++ EstudoPy/oppPython/game/player.py
@@ -1,10 +1,23 @@
 class Player(object):
-    def __int__(self, name):
+    def __init__(self, name):
         self.name = name
-        self.lives = 3
+        self._lives = 3
         self.level = 1
         self.score = 0
 
+    def _get_lives(self):
+        return self._lives
 
-    
+    def _set_lives(self, lives):
+        if lives >= 0:
+            self._lives = lives
+        else:
+            print("Lives can not be negative")
+            self._lives = 0
 
+    lives = property(_get_lives, _set_lives)
+
+    def __str__(self) -> str:
+        return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score {0.score}".format(
+            self
+        )
