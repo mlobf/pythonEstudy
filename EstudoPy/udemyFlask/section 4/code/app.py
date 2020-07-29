f705da332f07cb96fa8329d427eb961cee11a112
marcos <marcoslemeborbafilho@gmail.com>
2020-07-28 23:43:14 -0300

    commit da madruga

--- EstudoPy/udemyFlask/section 4/code/app.py
+++ EstudoPy/udemyFlask/section 4/code/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jose'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
         # Send the Correct Item to broswer, no more need to use jsonify --Flask-RESTful does it
         item = next(filter(lambda x: x["name"] == name, items), None)
--- EstudoPy/udemyFlask/section 4/code/security.py
+++ EstudoPy/udemyFlask/section 4/code/security.py
@@ -1,3 +1,4 @@
+from werkzeug.security import safe_str_cmp
 from user import User
 
 
@@ -10,7 +11,7 @@ userid_mapping = {u.id: u for u in users}
 
 def authenticate(username, password):
     user = username_mapping.get(username, None)
-    if user and user.password == password:
+    if user and safe_str_cmp(user.password == password):
         return user
 
 
