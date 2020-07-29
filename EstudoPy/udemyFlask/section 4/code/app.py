c8ce36172993214203a14ddd674095bed4882bfc
marcos <marcoslemeborbafilho@gmail.com>
2020-07-28 23:25:55 -0300

    commit da noite

--- EstudoPy/udemyFlask/section 4/code/app.py
+++ EstudoPy/udemyFlask/section 4/code/app.py
@@ -1,18 +1,40 @@
-from flask import Flask from flask_restful import Resource, Api
+from flask import Flask, request
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+app.secret_key = 'jose'
 api = Api(app)
 
 
-class Student(Resource):
-    
+items = []
+
+
+class Item(Resource):
     def get(self, name):
-        return {"student": name}
+        # Send the Correct Item to broswer, no more need to use jsonify --Flask-RESTful does it
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"item": item}, 200 if item else 404
+
+    def post(self, name):  # Create Item under REST perspective
+        if next(filter(lambda x: x["name"] == name, items), None):
+            return {"message": "An item with name {} already exists.".format(name)}, 400
+
+        data = request.get_json(force=True)
+        item = {"name": name, "price": data["price"]}
+        items.append(item)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": items}
 
 
 #    def post(self):
 
-api.add_resource(Student, "/student/<string:name>")
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+
 
 app.run(port=5000, debug=True)
