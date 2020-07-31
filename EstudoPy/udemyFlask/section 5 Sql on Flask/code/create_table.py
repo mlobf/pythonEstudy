8e7980f01414a1b11300d54189c48843a0dfd534
marcos <marcoslemeborbafilho@gmail.com>
2020-07-30 22:00:18 -0300

    commit

---
+++ EstudoPy/udemyFlask/section 5 Sql on Flask/code/create_table.py
@@ -0,0 +1,14 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
+
+
+create_table = "CREATE TABLE IF NOT EXIST users (id INTEGER PRIMARY KEY, username text, password text)"
+cursor.execute(create_table)
+
+connection.commit()
+
+connection.close()
