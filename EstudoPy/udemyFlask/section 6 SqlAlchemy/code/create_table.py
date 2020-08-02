8d4eb39f969996b11c197264794bfcd989422009
marcos <marcoslemeborbafilho@gmail.com>
2020-08-02 13:39:48 -0300

    sqlAlchemy began

---
+++ EstudoPy/udemyFlask/section 6 SqlAlchemy/code/create_table.py
@@ -0,0 +1,15 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+
+connection = sqlite3.connect("data.db")
+cursor = connection.cursor()
+
+
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+
+cursor.execute(create_table)
+
+connection.commit()
+
+connection.close()
