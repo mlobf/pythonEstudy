9c94a9e40297f52238244c8a572fdffe7faf76f2
marcos <marcoslemeborbafilho@gmail.com>
2020-08-28 23:38:39 -0300

    teste on tdd

---
+++ EstudoPy/django3UdemyCourse/password_generador/password_generador/tests/test.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from generator.views import home, password, about
+
+class ViewTest(TestCase):
+
+    def test_home(self):
+        pass
+
+    def test_password(self):
+        pass
+
+    def test_about(self):
+        pass
